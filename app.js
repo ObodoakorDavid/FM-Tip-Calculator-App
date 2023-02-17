293ed4edaf4b91170834da2d84e044f50276f068
ObodoakorDavid <obodoakordavid@outlook.com>
2023-02-17 06:10:31 +0100

    fix: fixed the percentage button

--- app.js
+++ app.js
@@ -6,32 +6,34 @@ let buttons = document.querySelectorAll(".grid-container button");
 let inputPercent = document.querySelector(".grid-container input");
 let billError = document.querySelector(".section1 > small");
 let numberOfPeopleError = document.querySelector(".section3 > small");
-// console.log(buttons);
+let tipPersonElement = document.querySelector(".tip-person");
+let tipTotalElement = document.querySelector(".tip-total");
 let numberOfPeopleElement = document.querySelector(".nop");
 let form = document.querySelector("form");
 let percent = null;
 let bill = null;
 let numberOfPeopleInput = null;
 let tip = null;
-let tipAmountValue = 0;
-let totalAmountValue = 0;
+// let tipAmountValue = 0;
+// let totalAmountValue = 0;
 
 billError.style.display = "none";
 numberOfPeopleError.style.display = "none";
 
 function calcPercent() {
-  percent = Number(numberOfPeopleInput);
   let tempResult = (bill * percent) / 100;
   let tipPerPerson = tempResult / numberOfPeopleInput;
-  tipAmountValue = tipPerPerson;
+  // tipAmountValue = tipPerPerson;
   let totalResult = tipPerPerson * numberOfPeopleInput;
-  totalAmountValue = totalResult;
+  // totalAmountValue = totalResult;
 
-  console.log(result);
-  console.log("hELLO");
+  tipPersonElement.textContent = `$${tipPerPerson.toString().slice(0, 3)}`;
+  tipTotalElement.textContent = `$${totalResult}`;
+
   console.log(numberOfPeopleInput);
   console.log(percent);
   console.log(bill);
+  console.log(totalResult);
 }
 
 let checkErrorState = () => {
@@ -64,6 +66,19 @@ let checkErrorState = () => {
       billError.textContent = "Required!";
       billError.style.display = "block";
     }
+  } else if (numberOfPeopleInput == "" || bill == "") {
+    if (bill == "" && numberOfPeopleInput == "") {
+      billError.textContent = "Required!";
+      billError.style.display = "block";
+      numberOfPeopleError.textContent = "Required!";
+      numberOfPeopleError.style.display = "block";
+    } else if (numberOfPeopleInput == "") {
+      numberOfPeopleError.textContent = "Required!";
+      numberOfPeopleError.style.display = "block";
+    } else if (bill == "") {
+      billError.textContent = "Required!";
+      billError.style.display = "block";
+    }
   } else {
     calcPercent();
   }
@@ -71,14 +86,26 @@ let checkErrorState = () => {
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    buttons.forEach(btn => btn.classList.remove('active'))
     let input = e.target.value;
     percent = e.target.value;
     console.log(input);
+    checkErrorState();
+    button.classList.add('active')
+  });
+  button.addEventListener("focusout", () => {
+    checkErrorState();
   });
 });
 
 inputPercent.addEventListener("change", (e) => {
   percent = e.target.value;
+  checkErrorState();
+});
+
+inputPercent.addEventListener("focusout", (e) => {
+  percent = e.target.value;
+  checkErrorState();
 });
 
 billInput.addEventListener("change", (e) => {
@@ -89,6 +116,24 @@ billInput.addEventListener("change", (e) => {
 numberOfPeopleElement.addEventListener("change", (e) => {
   numberOfPeopleInput = e.target.value;
   checkErrorState();
+});
+
+// Fous State
+
+billInput.addEventListener("focusout", (e) => {
+  checkErrorState();
+});
+
+numberOfPeopleElement.addEventListener("focusout", (e) => {
+  checkErrorState();
+});
+
+billInput.addEventListener("focusin", (e) => {
+  billError.style.display = "none";
+});
+
+numberOfPeopleElement.addEventListener("focusin", (e) => {
+  numberOfPeopleError.style.display = "none";
 });
 
 form.addEventListener("submit", (e) => {
